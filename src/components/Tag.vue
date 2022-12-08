efef330ef65bd0fa9f0a519576b3e021caf31fc9
Tobi Abiodun <abiodun.tobi@gmail.com>
2022-12-08 09:21:07 -0500

    Fix linting and finish adding page for filters

---
+++ src/components/Tag.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="tag flex items-center px-1 justify-center text-xs">
+    <ccu-icon
+      :alt="$t('actions.cancel')"
+      v-if="closeable"
+      size="xxs"
+      class="mx-1"
+      type="cancel"
+      @click="$emit('closed')"
+    />
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Tag',
+  props: {
+    closeable: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.tag {
+  @apply border bg-white;
+  font-weight: lighter;
+}
+
+.close {
+  margin-top: 1px;
+  cursor: pointer;
+}
+
+.close:after {
+  content: '\00d7';
+  font-weight: lighter;
+  font-size: 15px;
+  opacity: 0.5;
+}
+</style>
