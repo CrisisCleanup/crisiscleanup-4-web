a28507fceb2f47504ca3e7192bb53ae3c45e6f5e
Deep Panchal <deep302001@gmail.com>
2022-12-11 12:06:04 -0600

    --wip-- [skip ci]

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,15 +9,48 @@ module.exports = {
     'eslint:recommended',
     'plugin:vue/vue3-essential',
     'plugin:prettier/recommended',
+    'plugin:vue/vue3-recommended',
     // show eslint errors/warnings in the editor
     path.join(__dirname, 'node_modules/xo/config/plugins.cjs'),
   ],
   overrides: [],
   parserOptions: {
+    parser: '@typescript-eslint/parser',
     ecmaVersion: 'latest',
     sourceType: 'module',
     project: ['tsconfig.json'],
   },
+  parser: 'vue-eslint-parser',
+  settings: {
+    'import/extensions': ['.js', '.mjs', '.jsx', '.ts', '.tsx'],
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.mjs', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
   plugins: ['vue', '@typescript-eslint'],
-  rules: {},
+  rules: {
+    '@typescript-eslint/naming-convention': [
+      'warn',
+      {
+        selector: 'variable',
+        format: ['camelCase'],
+      },
+    ],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        mjs: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+  },
 };
