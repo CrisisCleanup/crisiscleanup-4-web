67d37f3d7a647c25481482e45db6e3f0d9fe994c
Tobi Abiodun <abiodun.tobi@gmail.com>
2022-12-19 10:58:44 -0500

    Implement Location Creation Tool

---
+++ src/models/Layer.ts
@@ -0,0 +1,24 @@
+import { Model } from '@vuex-orm/core';
+
+export default class Layer extends Model {
+  static entity = 'layers';
+
+  static fields() {
+    return {
+      id: this.attr(''),
+      title: this.string(''),
+      type: this.attr(null),
+      description: this.string(''),
+      locations: this.attr([]),
+      available_to: this.attr([]),
+    };
+  }
+
+  static apiConfig = {
+    actions: {
+      fetchById(id: string) {
+        return this.get(`/layers/${id}`);
+      },
+    } as any,
+  };
+}
