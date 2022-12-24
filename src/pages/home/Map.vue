17dcf69a4fc0fe398fdc466c65b26dbabc213549
Tobi Abiodun <abiodun.tobi@gmail.com>
2022-12-24 12:12:25 -0500

    Add a bunch of stattic pages alongside worksite public map

---
+++ src/pages/home/Map.vue
@@ -0,0 +1,222 @@
+<template>
+  <Home>
+    <div class="grid--main">
+      <div class="text-4xl">
+        {{ $t('mapVue.visualize_disaster') }}
+      </div>
+      <div class="text-xl">
+        {{ $t('mapVue.choose_incident_dropdown') }}
+      </div>
+
+      <base-select
+        :model-value="selectedIncident"
+        class="form-field"
+        :options="incidents"
+        style="z-index: 10000; position: relative"
+        searchable
+        select-classes="bg-white outline-none h-12 mb-3 w-72"
+        item-key="id"
+        label="name"
+        :placeholder="$t('mapVue.disaster')"
+        @update:modelValue="
+          (value) => {
+            selectedIncident = value;
+            reloadMap();
+            setLocations(value);
+          }
+        "
+      />
+
+      <div class="h-120 border bg-crisiscleanup-light-grey relative">
+        <SimpleMap
+          :map-loading="false"
+          :available-work-types="availableWorkTypes"
+          :show-legend="Object.keys(availableWorkTypes)"
+        />
+      </div>
+
+      <div class="text-lg my-1">
+        {{ $t('mapVue.personal_info_hidden') }}
+      </div>
+    </div>
+  </Home>
+</template>
+
+<script>
+import axios from 'axios';
+import { reactive, toRefs, onMounted } from 'vue';
+import * as L from 'leaflet';
+import Home from '@/layouts/Home.vue';
+import SimpleMap from '@/components/SimpleMap.vue';
+import useWorksiteMap from '@/hooks/worksite/useWorksiteMap';
+
+export default {
+  name: 'Map',
+  components: { Home, SimpleMap },
+  setup() {
+    let mapUtils;
+    const state = reactive({
+      incidents: [],
+      locations: [],
+      workTypeMap: {},
+      statusMap: {},
+      selectedIncident: null,
+      availableWorkTypes: {},
+    });
+
+    async function setLocations(incidentId) {
+      const incident = state.incidents.find((i) => i.id === incidentId);
+      if (incident.locations.length > 0) {
+        const locationIds = incident.locations.map(
+          (location) => location.location,
+        );
+        const locationsResponse = await axios.get(
+          `${
+            import.meta.env.VITE_APP_API_BASE_URL
+          }/locations?id__in=${locationIds.join(',')}`,
+        );
+        state.locations = locationsResponse.data.results;
+      } else {
+        state.locations = [];
+      }
+
+      mapUtils?.removeLayer('public_incident_objects');
+
+      for (const location of state.locations) {
+        const geojsonFeature = {
+          type: 'Feature',
+          properties: location.attr,
+          geometry: location.poly || location.geom || location.point,
+        };
+        const geojsonLayer = L.geoJSON(geojsonFeature);
+        const [layer] = geojsonLayer.getLayers();
+        layer.key = 'public_incident_objects';
+        layer.addTo(mapUtils.getMap());
+      }
+    }
+    function displayWorksite(worksite) {
+      const popup = L.popup({ className: 'pixi-popup' });
+      let popupContent = `<b>${worksite.address} (${worksite.case_number}</b>)`;
+
+      for (const worktype of worksite.work_types) {
+        popupContent += `<div>${state.workTypeMap[worktype.work_type]}(${
+          state.statusMap[worktype.status]
+        })</div>`;
+      }
+
+      popup
+        .setLatLng([worksite.x, worksite.y])
+        .setContent(popupContent)
+        .openOn(mapUtils.getMap());
+    }
+
+    async function loadCases() {
+      const response = await axios.get(
+        `${import.meta.env.VITE_APP_API_BASE_URL}/worksites_public`,
+        {
+          params: { incident: state.selectedIncident },
+        },
+      );
+
+      const markers = response.data.results;
+      return markers;
+    }
+
+    async function reloadMap() {
+      const cases = await loadCases();
+      mapUtils?.reloadMap(
+        cases,
+        cases.map((m) => m.id),
+      );
+    }
+
+    onMounted(async () => {
+      const incidentsResponse = await axios.get(
+        `${
+          import.meta.env.VITE_APP_API_BASE_URL
+        }/incidents_list?fields=id,name,short_name,geofence,locations,turn_on_release&limit=200&sort=-start_at`,
+      );
+      const workTypesResponse = await axios.get(
+        `${import.meta.env.VITE_APP_API_BASE_URL}/work_types`,
+      );
+      const statusResponse = await axios.get(
+        `${import.meta.env.VITE_APP_API_BASE_URL}/statuses`,
+      );
+      state.incidents = incidentsResponse.data.results;
+      state.workTypeMap = workTypesResponse.data.results.reduce(function (
+        map,
+        obj,
+      ) {
+        map[obj.key] = obj.name_t;
+        return map;
+      },
+      {});
+      state.statusMap = statusResponse.data.results.reduce(function (map, obj) {
+        map[obj.status] = obj.status_name_t;
+        return map;
+      }, {});
+      state.selectedIncident = state.incidents[0].id;
+      const markers = await loadCases();
+
+      mapUtils = useWorksiteMap(
+        markers,
+        markers.map((m) => m.id),
+        displayWorksite,
+        ({ workTypes }) => {
+          state.availableWorkTypes = workTypes;
+        },
+      );
+
+      await setLocations(state.incidents[0].id);
+    });
+    return {
+      ...toRefs(state),
+      setLocations,
+      reloadMap,
+    };
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.homegrid {
+  &.grid-container {
+    grid-template-areas:
+      'logo . . . . survivors'
+      'nav main main main main main'
+      'nav main main main main main'
+      'nav main main main main main'
+      'actions . . . . .'
+      '. . . . . .'
+      '. . . . footer footer';
+
+    .grid {
+      &--main {
+        @apply mx-16;
+      }
+      &--overlay {
+        grid-row: 1 / span 7;
+      }
+    }
+  }
+}
+@media only screen and (max-width: 640px) {
+  .homegrid {
+    height: 100vh;
+    &.grid-container {
+      grid:
+        [r1] 'logo survivors' [r1end]
+        [r2] 'nav nav' [r2end]
+        [r3] 'actions actions' [r3end]
+        [r4] 'footer footer' [r4end]
+        [r5] 'main main' [r5end]
+        [r6] 'main main' [r6end]
+        / auto;
+      grid-template-rows: 1fr 1fr 1fr 1fr;
+    }
+  }
+}
+.input {
+  @apply m-2;
+}
+</style>
---
+++ src/pages/home/Training.vue
@@ -0,0 +1,201 @@
+<template>
+  <Home>
+    <div class="grid--main mb-60 sm:mb-0">
+      <!--- Title --->
+      <div class="flex flex-col m-5 pb-8">
+        <h1 class="text-5xl px-0.5 text-crisiscleanup-dark-500 font-bold">
+          {{ $t('training.training') }}
+        </h1>
+        <!--- Description --->
+        <p class="text-xl px-0.5">
+          {{ $t('training.please_complete_training') }}
+        </p>
+      </div>
+      <!--- Content --->
+      <div class="flex flex-col sm:flex-row justify-between sm:m-5">
+        <!--- Training Video --->
+        <div class="flex-col sm:w-1/2">
+          <div class="video-container">
+            <iframe
+              class="resp-video"
+              width="100%"
+              height="100%"
+              src="https://www.youtube.com/embed/ot4LZjtK0xo"
+              frameborder="0"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            />
+          </div>
+        </div>
+        <!--- Training Video Description --->
+        <div class="flex-col sm:w-1/2">
+          <h1 class="px-10 text-3xl font-bold text-crisiscleanup-dark-500">
+            {{ $t('training.mandatory_training_video') }}
+          </h1>
+          <p class="px-10 text-xl text-crisiscleanup-dark-400">
+            {{ $t('training.mandatory_training_video_intro') }}
+          </p>
+        </div>
+        <!--- /Training Video Description --->
+      </div>
+      <!--- /Content --->
+
+      <!--- Content --->
+      <div class="flex flex-col sm:flex-row justify-between sm:m-5">
+        <!--- Training Video --->
+        <div class="flex-col sm:w-1/2">
+          <div class="video-container">
+            <iframe
+              class="resp-video"
+              width="100%"
+              height="100%"
+              src="https://www.youtube.com/embed/dp_1A1qVSog"
+              frameborder="0"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            />
+          </div>
+        </div>
+        <!--- Training Video Description --->
+        <div class="flex-col sm:w-1/2">
+          <h1 class="px-10 text-3xl font-bold text-crisiscleanup-dark-500">
+            {{ $t('training.suppliment_phone_training') }}
+          </h1>
+          <p class="px-10 text-xl text-crisiscleanup-dark-400">
+            {{ $t('training.suppliment_phone_training_intro') }}
+          </p>
+        </div>
+        <!--- /Training Video Description --->
+      </div>
+      <!--- /Content --->
+
+      <!--- Content --->
+      <div class="flex flex-col sm:flex-row justify-between m-5">
+        <!--- Training Video --->
+        <div class="flex-col sm:w-1/2">
+          <div class="video-container">
+            <iframe
+              class="resp-video"
+              width="100%"
+              height="100%"
+              src="https://www.youtube.com/embed/HqXm2s5Dy3c"
+              frameborder="0"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            />
+          </div>
+        </div>
+        <!--- Training Video Description --->
+        <div class="flex-col sm:w-1/2">
+          <h1 class="px-10 text-3xl font-bold text-crisiscleanup-dark-500">
+            {{ $t('training.calldown_training') }}
+          </h1>
+          <p class="px-10 text-xl text-crisiscleanup-dark-400">
+            {{ $t('training.calldown_training_intro') }}
+          </p>
+        </div>
+        <!--- /Training Video Description --->
+      </div>
+      <!--- /Content --->
+
+      <!--- Content --->
+      <div class="flex flex-col sm:flex-row justify-between m-5">
+        <!--- Training Video --->
+        <div class="flex-col sm:w-1/2">
+          <div class="video-container">
+            <iframe
+              class="resp-video"
+              width="100%"
+              height="100%"
+              src="https://www.youtube.com/embed/vWFI7udFmik"
+              frameborder="0"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            />
+          </div>
+        </div>
+        <!--- Training Video Description --->
+        <div class="flex-col sm:w-1/2">
+          <h1 class="px-10 text-3xl font-bold text-crisiscleanup-dark-500">
+            {{ $t('training.ccu_during_covid') }}
+          </h1>
+          <p class="px-10 text-xl text-crisiscleanup-dark-400">
+            {{ $t('training.ccu_during_covid_intro') }}
+          </p>
+        </div>
+        <!--- /Training Video Description --->
+      </div>
+      <!--- /Content --->
+      <home-footer />
+    </div>
+  </Home>
+</template>
+
+<script>
+import Home from '@/layouts/Home.vue';
+
+export default {
+  name: 'TrainingPage',
+  components: {
+    Home,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.homegrid {
+  &.grid-container {
+    grid:
+      [r1] 'logo . main main main main survivors' 20% [r1end]
+      [r2] 'nav . main main main main main' [r2end]
+      [r3] 'actions . main main main main main' [r3end]
+      [r4] '. . . . footer footer .' [r4end]
+      / auto;
+
+    .grid {
+      &--main {
+        align-self: center;
+        @apply sm:mx-10 h-full;
+        overflow: auto;
+        grid-row: main-start / span actions-end;
+      }
+      &--overlay {
+        grid-column: 1 / span 5;
+      }
+      &--footer {
+        height: auto;
+        display: flex;
+        justify-content: space-evenly;
+        @apply my-1;
+      }
+    }
+  }
+}
+@media only screen and (max-width: 640px) {
+  .homegrid {
+    height: 100vh;
+    &.grid-container {
+      grid:
+        [r1] 'logo survivors' [r1end]
+        [r4] 'nav nav' [r4end]
+        [r2] 'main main' [r2end]
+        [r3] 'actions actions' [r3end]
+        / auto;
+      grid-template-rows: 0.5fr 0.5fr 4fr 1fr;
+    }
+  }
+}
+.resp-video {
+  position: absolute;
+  top: 0;
+  left: 0;
+  border: 0;
+  width: 100%;
+  height: 100%;
+}
+.video-container {
+  position: relative;
+  overflow: hidden;
+  padding-top: 56.25%;
+}
+</style>
