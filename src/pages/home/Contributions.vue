77eda2982402f7c2a485fbfcbaac66be5fe091b5
Tobi Abiodun <abiodun.tobi@gmail.com>
2023-01-08 13:15:16 -0500

    Add remaining unauthenticated pages

---
+++ src/pages/home/Contributions.vue
@@ -0,0 +1,100 @@
+<template>
+  <Home>
+    <div class="grid--main m-10">
+      <div class="text-4xl mb-5">
+        {{ $t('contrib.ccu_contributor_agreements') }}
+      </div>
+      <div class="row mb-6">
+        <base-text
+          v-html="$t('contrib.ccu_uses_agreements_for_contributions')"
+        />
+        <base-text v-html="$t('contrib.purpose_goals_of_agreements')" />
+        <base-text variant="h3">{{
+          $t('contrib.contributor_license_agreements')
+        }}</base-text>
+        <ul>
+          <li>
+            <a class="doc-link" href="/icla.pdf" target="_blank">{{
+              $t('contrib.icla')
+            }}</a>
+          </li>
+          <li>
+            <a class="doc-link" href="/ccla.pdf" target="_blank">{{
+              $t('contrib.ccla')
+            }}</a>
+          </li>
+        </ul>
+        <base-text v-html="$t('contrib.everyone_should_sign_cla')" />
+        <base-text v-html="$t('contrib.purpose_of_cla')" />
+        <base-text v-html="$t('contrib.corporations_sign_ccla')" />
+        <base-text v-html="$t('contrib.developers_sign_icla_even_with_ccla')" />
+        <base-text v-html="$t('contrib.someone_with_authority_signs_ccla')" />
+        <base-text v-html="$t('contrib.icla_independent_of_employer')" />
+        <base-text variant="h3">{{
+          $t('contrib.software_license_grant')
+        }}</base-text>
+        <ul>
+          <li>
+            <a class="doc-link" href="/sgla.pdf" target="_blank">{{
+              $t('contrib.sgla')
+            }}</a>
+          </li>
+        </ul>
+        <base-text v-html="$t('contrib.sgla_required_for_code_donation')" />
+        <base-text variant="h3">{{ $t('contrib.how_to_submit') }}</base-text>
+        <base-text v-html="$t('contrib.must_be_emailed')" />
+        <base-text v-html="$t('contrib.complete_scan_all_pages_then_email')" />
+        <base-text v-html="$t('contrib.please_use_email_in_doc')" />
+        <base-text v-html="$t('contrib.attachments_only_no_links')" />
+        <base-text v-html="$t('contrib.define_signing')" />
+      </div>
+    </div>
+  </Home>
+</template>
+
+<script>
+import { useI18n } from 'vue-i18n';
+import Home from '@/layouts/Home.vue';
+
+export default {
+  name: 'Contributions',
+  components: { Home },
+  setup() {
+    const { t } = useI18n();
+
+    const contrib = {
+      title: t('nav.contributions'),
+      subtitle1_intro: t('nav.contributions'),
+    };
+
+    return {
+      contrib,
+    };
+  },
+};
+</script>
+
+<style scoped>
+@import '../../assets/css/_terms-privacy.scss';
+.doc-link {
+  @apply text-base;
+}
+.homegrid {
+  &.grid-container {
+    grid-template-areas:
+      'logo . . . . survivors'
+      'nav . main main main .'
+      'nav . main main main .'
+      'actions actions main main main .'
+      '. . main main main .';
+    overflow: auto;
+
+    .grid {
+      &--overlay {
+        grid-row: 1 / span 5;
+        grid-column: 1 / span 3;
+      }
+    }
+  }
+}
+</style>
---
+++ src/pages/home/RegisterOrganization.vue
@@ -0,0 +1,365 @@
+<template>
+  <Home>
+    <div class="grid--main m-10">
+      <div class="text-4xl">{{ $t('registerOrg.register_org') }}</div>
+      <div class="text-2xl w-3/4">
+        {{ $t('registerOrg.please_create_profile') }}
+      </div>
+      <form
+        ref="form"
+        class="form w-full sm:w-4/5 mt-6"
+        @submit.prevent="register"
+      >
+        <ol class="">
+          <li class="text-xl form-item">
+            {{ $t('registerOrg.choose_a_disaster') }}
+            <base-select
+              :model-value="organization.incident"
+              class="m-3"
+              :options="incidents"
+              searchable
+              select-classes="bg-white h-12 mb-3 max-w-sm"
+              item-key="id"
+              label="name"
+              :placeholder="$t('registerOrg.disaster')"
+              @update:modelValue="updateOrganizationIncident"
+            />
+          </li>
+
+          <li class="text-xl form-item">
+            {{ $t('registerOrg.org_info') }}
+            <div class="text-base w-3/5">
+              {{ $t('registerOrg.use_local_org_name_msg') }}
+            </div>
+            <div class="flex flex-wrap">
+              <OrganizationSearchInput
+                size="large"
+                class="form-field"
+                allow-new
+                @selectedOrganization="
+                  (organization) => {
+                    $router.push(
+                      `/request_access?organization=${organization.name}`,
+                    );
+                  }
+                "
+                @input="organization.name = $event"
+              />
+              <base-input
+                v-model="organization.url"
+                type="text"
+                class="form-field"
+                size="large"
+                :placeholder="$t('registerOrg.website')"
+              />
+              <base-input
+                v-model="organization.facebook"
+                class="form-field"
+                size="large"
+                :placeholder="$t('registerOrg.facebook')"
+              />
+              <base-input
+                v-model="organization.twitter"
+                class="form-field"
+                size="large"
+                :placeholder="$t('registerOrg.twitter')"
+              />
+            </div>
+            <textarea
+              v-model="organization.where_are_you_working"
+              class="text-base w-4/5 border border-crisiscleanup-dark-100 placeholder-crisiscleanup-dark-200 outline-none m-3 p-2 resize-none"
+              rows="4"
+              :placeholder="$t('registerOrg.where_working')"
+              required
+            />
+            <textarea
+              v-model="organization.referral"
+              class="text-base w-4/5 border border-crisiscleanup-dark-100 placeholder-crisiscleanup-dark-200 outline-none m-3 p-2 resize-none"
+              rows="4"
+              :placeholder="$t('registerOrg.referral')"
+              required
+            />
+          </li>
+
+          <li class="text-xl form-item">
+            {{ $t('registerOrg.primary_contact') }}
+            <div class="flex flex-wrap">
+              <base-input
+                v-model="user.first_name"
+                type="text"
+                class="form-field"
+                size="large"
+                :placeholder="$t('registerOrg.first_name')"
+                required
+              />
+              <base-input
+                v-model="user.last_name"
+                type="text"
+                class="form-field"
+                size="large"
+                :placeholder="$t('registerOrg.last_name')"
+                required
+              />
+              <base-input
+                v-model="user.email"
+                type="email"
+                class="form-field"
+                size="large"
+                :placeholder="$t('registerOrg.email')"
+                required
+              />
+              <base-input
+                v-model="user.mobile"
+                class="form-field"
+                size="large"
+                :validator="validatePhoneNumber"
+                :placeholder="$t('registerOrg.cell_phone_number')"
+                required
+              />
+            </div>
+          </li>
+
+          <li class="text-xl form-item">
+            {{ $t('registerOrg.org_roles') }}
+            <div
+              class="text-base w-3/5"
+              v-html="$t('registerOrg.capability_explanation')"
+            ></div>
+            <CapabilityGrid
+              class="text-sm mt-3"
+              @updated="
+                (matrix) => {
+                  updatedOrganizationCapabilitiesMatrix = matrix;
+                }
+              "
+            />
+          </li>
+
+          <base-checkbox
+            v-model="organization.publish"
+            class="text-base activities-checkbox"
+          >
+            <div>
+              {{ $t('registerOrg.publish_profile') }}
+            </div>
+          </base-checkbox>
+
+          <base-checkbox
+            v-model="organization.accepted_terms"
+            class="text-base activities-checkbox"
+            required
+          >
+            <div
+              class="privacy"
+              v-html="$t('registerOrg') ? $t('registerOrg.tos_priv_agree') : ''"
+            ></div>
+          </base-checkbox>
+        </ol>
+        <base-button
+          size="large"
+          class="px-5 py-2 m-1 self-center md:w-108 lg:w-108 m-auto mb-20 sm:w-full"
+          variant="solid"
+          :text="$t('actions.sign_up')"
+          :alt="$t('actions.sign_up')"
+          :action="register"
+        />
+      </form>
+    </div>
+  </Home>
+</template>
+
+<script>
+import { useI18n } from 'vue-i18n';
+import { nextTick, reactive, toRefs } from 'vue';
+import { useToast } from 'vue-toastification';
+import { useRouter } from 'vue-router';
+import axios from 'axios';
+import Organization from '@/models/Organization';
+import { getErrorMessage } from '@/utils/errors';
+import OrganizationSearchInput from '@/components/OrganizationSearchInput.vue';
+import Home from '@/layouts/Home.vue';
+import CapabilityGrid from '@/components/CapabilityGrid.vue';
+import useCapabilities from '@/hooks/useCapabilities';
+
+export default {
+  name: 'Register',
+  components: {
+    Home,
+    CapabilityGrid,
+    OrganizationSearchInput,
+  },
+
+  setup() {
+    const { t } = useI18n();
+    const form = ref(null);
+    const state = reactive({
+      organization: {
+        name: '',
+        url: '',
+        type_t: '',
+        facebook: '',
+        twitter: '',
+        referral: '',
+        publish: false,
+        does_damage_assessment: false,
+        does_intake_assessment: false,
+        does_cleanup: false,
+        does_follow_up: false,
+        does_minor_repairs: false,
+        does_rebuilding: false,
+        does_coordination: false,
+        government: false,
+        does_other_activity: false,
+        where_are_you_working: '',
+        review_other_organizations: false,
+        accepted_terms: false,
+        not_an_org: false,
+        incident: null,
+      },
+      user: {
+        first_name: '',
+        last_name: '',
+        email: '',
+        mobile: '',
+      },
+      incidents: [],
+      organizationCapabilities: [],
+      updatedOrganizationCapabilitiesMatrix: null,
+      selectedIncidentId: null,
+      organizationTypes: [
+        'orgType.survivor_client_services',
+        'orgType.voad',
+        'orgType.coad',
+        'orgType.government',
+        'orgType.ltr',
+        'orgType.coalition',
+      ].map((key) => {
+        return { key, label: t(key) };
+      }),
+    });
+    const $toasted = useToast();
+    const { saveCapabilities } = useCapabilities();
+    const router = useRouter();
+
+    async function register() {
+      const isValid = form.value.reportValidity();
+      if (!isValid) {
+        return;
+      }
+
+      try {
+        const savedOrganization = await Organization.api().post(
+          '/organizations',
+          {
+            ...state.organization,
+            contact: { ...state.user },
+          },
+        );
+        [state.organization] = savedOrganization.entities.organizations;
+        await saveCapabilities(
+          state.updatedOrganizationCapabilitiesMatrix,
+          state.organizationCapabilities,
+          state.organization,
+          false,
+        );
+        $toasted.success(t('registerOrg.org_registration_success'), {
+          duration: 7000,
+        });
+      } catch (error) {
+        await $toasted.error(getErrorMessage(error));
+        return;
+      }
+      await router.push('/');
+    }
+    function updateOrganizationIncident(value) {
+      nextTick(() => {
+        state.organization.incident = value;
+        state.organization = { ...state.organization };
+      });
+    }
+
+    onMounted(async () => {
+      const incidentsResponse = await axios.get(
+        `${
+          import.meta.env.VITE_APP_API_BASE_URL
+        }/incidents_list?fields=id,name&limit=200&sort=-start_at`,
+        {
+          headers: {
+            Authorization: null,
+          },
+        },
+      );
+      state.incidents = incidentsResponse.data.results;
+    });
+
+    return {
+      ...toRefs(state),
+      form,
+      register,
+      updateOrganizationIncident,
+    };
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.homegrid {
+  &.grid-container {
+    grid-template-areas:
+      'logo . . . . survivors'
+      'nav . main main main main'
+      'nav . main main main main'
+      'actions actions main main main main'
+      '. . main main main main';
+    overflow: auto;
+
+    .grid {
+      &--overlay {
+        grid-row: 1 / span 5;
+        grid-column: 1 / span 3;
+      }
+    }
+  }
+}
+
+.form-field {
+  @apply w-full sm:w-2/5 m-3;
+}
+
+.form-item {
+  @apply py-3 ml-6;
+}
+
+.activities-checkbox {
+  @apply pl-12 my-6;
+}
+
+@media only screen and (max-width: 640px) {
+  .homegrid {
+    height: 100vh;
+    &.grid-container {
+      grid-template-areas:
+        'logo'
+        'main'
+        'survivors'
+        'nav'
+        'actions';
+      grid-template-rows: 0.5fr 0.5fr 4fr 1fr;
+    }
+  }
+
+  .form-field {
+    @apply w-full;
+  }
+}
+</style>
+
+<style lang="scss" scoped>
+.privacy > a {
+  @apply text-primary-dark;
+  text-decoration: underline !important;
+  &:hover {
+    text-decoration: none;
+  }
+}
+</style>
