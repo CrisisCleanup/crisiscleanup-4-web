7011f2a53f8b9fb28c4022412a359190a577c6a3
Deep Panchal <deep302001@gmail.com>
2023-01-20 10:41:57 -0600

    feat(pages): add `NotFound` 404 page, update router.ts

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,30 @@
+<template>
+  <HomeLayout>
+    <div class="text-2xl text-center mb-3">{{ t('~~404_not_found') }}</div>
+    <div class="cover-image"></div>
+  </HomeLayout>
+</template>
+
+<script lang="ts">
+import HomeLayout from '@/layouts/Home.vue';
+
+export default defineComponent({
+  name: 'NotFound',
+  components: { HomeLayout },
+  setup() {
+    const { t } = useI18n();
+    return {
+      t,
+    };
+  },
+});
+</script>
+
+<style scoped lang="scss">
+.cover-image {
+  background-image: url('https://source.unsplash.com/random');
+  background-size: cover;
+  background-repeat: no-repeat;
+  @apply w-full h-full;
+}
+</style>
