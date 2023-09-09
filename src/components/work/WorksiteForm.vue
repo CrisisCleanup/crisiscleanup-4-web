2aa7f5d475c2610cfde8c4ad4e2f23639019a843
Deep Panchal <deep302001@gmail.com>
2023-09-09 11:37:47 -0500

    fix(comps): set getter func on watch calls with props.* as they were not running

--- src/components/work/WorksiteForm.vue
+++ src/components/work/WorksiteForm.vue
@@ -1345,11 +1345,14 @@ export default defineComponent({
       { immediate: true },
     );
 
-    watch(props.worksiteId, (newValue) => {
-      if (!newValue) {
-        worksite.value = {};
-      }
-    });
+    watch(
+      () => props.worksiteId,
+      (newValue) => {
+        if (!newValue) {
+          worksite.value = {};
+        }
+      },
+    );
 
     watch(isWrongLocation, (newValue) => {
       if (newValue !== shouldSelectOnMap.value) {
