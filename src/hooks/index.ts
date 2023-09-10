3c9d053a28aafdb71f442c8c43bc9c18a2316c81
Braden Mars <bradenmars@bradenmars.me>
2023-09-10 01:28:37 -0500

    feat(hooks): fix `useCurrentUser` export, add `useAuth`
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,9 +1,10 @@
 export * from './phone';
 export * from './useAcl';
 export * from './useConnectFirst';
-export * from './useCurrentUser';
+export { default as useCurrentUser } from './useCurrentUser';
 export * from './useDialogs';
 export * from './useEmitter';
 export * from './useTabs';
 export * from './useValidation';
 export * from './useZendesk';
+export * from './useAuth';
