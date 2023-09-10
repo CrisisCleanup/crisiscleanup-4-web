ad35043a9fb9f8dbea5be04e5f8c7a1fcc603584
Braden Mars <bradenmars@bradenmars.me>
2023-09-10 01:35:41 -0500

    fix(login): reactively redirect from login page
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- src/pages/home/Login.vue
+++ src/pages/home/Login.vue
@@ -1,27 +1,47 @@
 <template>
   <div class="h-screen flex items-center justify-center">
+    <text>login</text>
     <spinner show-quote />
   </div>
 </template>
 
 <script lang="ts">
 import { AuthService } from '@/services/auth.service';
+import { useAuthStore } from '@/hooks/useAuth';
 import Spinner from '@/components/Spinner.vue';
 
 export default defineComponent({
   name: 'Login',
   components: { Spinner },
   setup() {
+    // todo: handle as route guard
     const router = useRouter();
-    const route = useRoute();
-    const store = useStore();
-    if (store.getters['auth/isLoggedIn']) {
-      router.push('/dashboard');
-    } else {
-      AuthService.buildOauthAuthorizationUrl(route.query.from).then((url) => {
-        window.location.href = url;
-      });
+
+    const authStore = useAuthStore();
+    authStore.getMe();
+
+    if (authStore.currentUser.value) {
+      router.push({ name: 'nav.dashboard_no_incident' });
     }
+
+    // authenticated
+    watch(
+      () => authStore.currentUser,
+      async (newUser) => {
+        console.log(authStore.currentUser.value);
+        console.log(newUser);
+        if (newUser) {
+          await router.push({
+            name: 'nav.dashboard_no_incident',
+          });
+        }
+      },
+      {
+        immediate: true,
+        flush: 'sync',
+      },
+    );
+
   },
 });
 </script>
