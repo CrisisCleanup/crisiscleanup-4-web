5090f2704413cecdd5370185399c93355c6a783a
Braden Mars <bradenmars@bradenmars.me>
2023-09-11 17:07:48 -0500

    feat(hooks/useCurrentUser): provide debounced updateCurrentUser and updateUserStates methods
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- src/hooks/useCurrentUser.ts
+++ src/hooks/useCurrentUser.ts
@@ -120,10 +120,19 @@ export default function useCurrentUser() {
     await updateCurrentUser({ states: newStates });
   };
 
+  const updateCurrentUserDebounced = useDebounceFn(updateCurrentUser, 300, {
+    maxWait: 1000,
+  });
+  const updateUserStatesDebounced = useDebounceFn(updateUserStates, 300, {
+    maxWait: 1000,
+  });
+
   return {
     currentUser,
     updateUserStates,
     updateCurrentUser,
+    updateCurrentUserDebounced,
+    updateUserStatesDebounced,
     userStates,
     userPreferences,
   };
