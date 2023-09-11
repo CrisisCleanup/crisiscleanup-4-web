bd4317c6482b2fd78e6445dcf319bf650eac0dd3
Braden Mars <bradenmars@bradenmars.me>
2023-09-11 18:51:50 -0500

    feat(modules): remove auth vuex store module
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,8 +2,6 @@ import { createStore } from 'vuex';
 import * as VuexORM from '@vuex-orm/core';
 import VuexORMAxios from '@vuex-orm/plugin-axios';
 import axios from 'axios';
-import { AuthService } from '../services/auth.service';
-import auth from './modules/auth';
 import incident from './modules/incident';
 import database from './database';
 import acl from './modules/acl';
@@ -11,7 +9,6 @@ import enums from './modules/enums';
 import locale from './modules/locale';
 import loading from './modules/loading';
 import events from './modules/events';
-
 // Import events from './modules/events';
 import phone from './modules/phone';
 // Import rc from './modules/rc';
@@ -28,11 +25,10 @@ VuexORM.use(VuexORMAxios, {
     Accept: 'application/json',
     'Content-Type': 'application/json',
   },
-  baseURL: `${import.meta.env.VITE_APP_API_BASE_URL as string}`,
+  baseURL: `${import.meta.env.VITE_APP_API_BASE_URL}`,
 });
 export const store = createStore<CCURootState>({
   modules: {
-    auth,
     acl,
     events,
     incident,
