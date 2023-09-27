5bf545d645136f14505e111135a3696d1cfb0b00
Deep Panchal <deep302001@gmail.com>
2023-09-27 15:50:07 -0500

    test(hooks): add more test cases for useCurrentIncident, fix issues with mocks

--- test/unit/hooks/useCurrentIncident.test.ts
+++ test/unit/hooks/useCurrentIncident.test.ts
@@ -4,33 +4,33 @@ import useCurrentIncident from '@/hooks/useCurrentIncident';
 
 const routeIncidentId = ref<string | undefined>();
 const userStateIncidentId = ref<number | undefined>();
+const useStoreMock = {
+  commit: vi.fn(),
+};
+const useCurrentUserMock = {
+  hasCurrentUser: ref(true),
+  updateUserStates: (state: Record<string, any>) => {
+    userStateIncidentId.value = state.incident as number;
+    return Promise.resolve(state);
+  },
+  currentUser: computed(() => {
+    return {
+      states: {
+        incident: userStateIncidentId.value,
+      },
+    };
+  }),
+};
 
 vi.mock('vuex', () => ({
   useStore() {
-    return {
-      commit: vi.fn(),
-    };
+    return useStoreMock;
   },
 }));
 vi.mock('@/hooks/useCurrentUser', async () => {
-  const actual = await vi.importActual('@/hooks/useCurrentUser');
   return {
-    ...actual,
     default() {
-      return {
-        hasCurrentUser: ref(true),
-        updateUserStates: (state: Record<string, any>) => {
-          userStateIncidentId.value = state.incident as number;
-          return Promise.resolve(state);
-        },
-        currentUser: computed(() => {
-          return {
-            states: {
-              incident: userStateIncidentId.value,
-            },
-          };
-        }),
-      };
+      return useCurrentUserMock;
     },
   };
 });
@@ -52,9 +52,9 @@ vi.mock('@/models/Incident', () => {
 
     static api() {
       return {
-        fetchById(id: number) {
+        fetchById: vi.fn((id: number) => {
           return { id };
-        },
+        }),
         addLocation: vi.fn(),
         removeLocation: vi.fn(),
       };
@@ -69,7 +69,9 @@ vi.mock('@/models/Incident', () => {
       };
     }
   }
-  return { default: MockIncidentModel };
+  return {
+    default: MockIncidentModel,
+  };
 });
 vi.mock('@vueuse/core', async () => {
   const actual = await vi.importActual('@vueuse/core');
@@ -106,5 +108,101 @@ describe('useCurrentIncident', () => {
     routeIncidentId.value = '456';
     await nextTick();
     expect(currentIncidentId.value).toBe(456);
+  });
+
+  it('should return hasCurrentIncident', async () => {
+    const { hasCurrentIncident } = useCurrentIncident();
+    expect(hasCurrentIncident.value).toBe(false);
+    routeIncidentId.value = '123';
+    await nextTick();
+    expect(hasCurrentIncident.value).toBe(true);
+  });
+
+  it('should resolve current incident from route', async () => {
+    const { currentIncident } = useCurrentIncident();
+    expect(currentIncident.value).toBe(undefined);
+    routeIncidentId.value = '123';
+    await nextTick();
+    expect(currentIncident.value).toMatchInlineSnapshot(`
+      {
+        "id": 123,
+      }
+    `);
+    routeIncidentId.value = '456';
+    await nextTick();
+    expect(currentIncident.value).toMatchInlineSnapshot(`
+      {
+        "id": 456,
+      }
+    `);
+  });
+
+  it('should resolve current incident from user state', async () => {
+    const { currentIncident } = useCurrentIncident();
+    expect(currentIncident.value).toBe(undefined);
+    userStateIncidentId.value = 111;
+    await nextTick();
+    expect(currentIncident.value).toMatchInlineSnapshot(`
+      {
+        "id": 111,
+      }
+    `);
+    routeIncidentId.value = undefined;
+    userStateIncidentId.value = 222;
+    await nextTick();
+    expect(currentIncident.value).toMatchInlineSnapshot(`
+      {
+        "id": 222,
+      }
+    `);
+  });
+
+  it('should prioritize incident from route over user state', async () => {
+    const { currentIncident } = useCurrentIncident();
+    expect(currentIncident.value).toBe(undefined);
+    routeIncidentId.value = '111';
+    await nextTick();
+    expect(currentIncident.value).toMatchInlineSnapshot(`
+      {
+        "id": 111,
+      }
+    `);
+    userStateIncidentId.value = 222;
+    await nextTick();
+    expect(currentIncident.value).toMatchInlineSnapshot(`
+      {
+        "id": 111,
+      }
+    `);
+
+    routeIncidentId.value = undefined;
+    await nextTick();
+    expect(currentIncident.value).toMatchInlineSnapshot(`
+      {
+        "id": 222,
+      }
+    `);
+  });
+
+  it('should commit incident id to store', async () => {
+    expect.assertions(1);
+    useCurrentIncident();
+    routeIncidentId.value = '123';
+    await nextTick();
+    expect(useStoreMock.commit).toHaveBeenCalledWith(
+      'incident/setCurrentIncidentId',
+      123,
+    );
+  });
+
+  it('should update user states', async () => {
+    expect.assertions(1);
+    vi.spyOn(useCurrentUserMock, 'updateUserStates');
+    useCurrentIncident();
+    routeIncidentId.value = '123';
+    await nextTick();
+    expect(useCurrentUserMock.updateUserStates).toHaveBeenCalledWith({
+      incident: 123,
+    });
   });
 });
