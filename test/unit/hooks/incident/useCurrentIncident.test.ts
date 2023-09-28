0296ba0d70fe6fff33ac42e6ac853af0d15ed131
Braden Mars <bradenmars@bradenmars.me>
2023-09-28 03:25:58 -0500

    test(hooks/incident): add `useCurrentIncident` unit test
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

---
+++ test/unit/hooks/incident/useCurrentIncident.test.ts
@@ -0,0 +1,62 @@
+import { useCurrentIncident } from '@/hooks';
+import { useStore } from 'vuex';
+import useCurrentUser from '@/hooks/useCurrentUser';
+import { useRouteIncident } from '@/hooks/incident/useRouteIncident';
+import { useUserIncident } from '@/hooks/incident/useUserIncident';
+import { useModelInstance } from '@/hooks/useModel';
+import Incident from '@/models/Incident';
+import { test, type Mock, vi, expect } from 'vitest';
+
+// Mocking necessary imports
+vi.mock('vuex');
+vi.mock('@/hooks/useCurrentUser');
+vi.mock('@/hooks/incident/useRouteIncident');
+vi.mock('@/hooks/incident/useUserIncident');
+vi.mock('@/hooks/useModel');
+vi.mock('@/models/Incident', () => ({
+  default: {
+    api: vi.fn(),
+  },
+}));
+
+test('hooks>>incident>>useCurrentIncident', async () => {
+  // Mocking return values
+  (useCurrentUser as Mock).mockReturnValue({ currentUser: { id: 1 } });
+  (useRouteIncident as Mock).mockReturnValue({ routeIncidentId: { value: 4 } });
+  (useUserIncident as Mock).mockReturnValue({
+    userIncidentId: {
+      value: 4,
+    },
+    updateUserIncident: vi.fn(),
+  });
+  (useModelInstance as Mock).mockReturnValue({
+    itemId: { value: 4 },
+    item: {},
+    isLoading: false,
+    hasItem: true,
+  });
+  Incident.api.mockReturnValue({
+    get: vi
+      .fn()
+      .mockResolvedValue({ response: { data: { results: [{ id: 4 }] } } }),
+  });
+
+  const store = { commit: vi.fn() };
+  (useStore as Mock).mockReturnValue(store);
+
+  const {
+    currentIncidentId,
+    currentIncident,
+    hasCurrentIncident,
+    isCurrentIncidentLoading,
+  } = useCurrentIncident();
+
+  console.log({
+    currentIncidentId: currentIncidentId.value,
+    currentIncident,
+    hasCurrentIncident,
+    isCurrentIncidentLoading,
+  });
+
+  expect(currentIncidentId.value).toBe(4);
+});
