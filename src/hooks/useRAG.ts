2a3d060f6759f350c16842420538be26c6e5ca1e
Braden Mars <bradenmars@bradenmars.me>
2023-12-02 04:41:03 -0600

    feat(hooks/useRAG): communicate via websocket for streaming RAG responses
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- src/hooks/useRAG.ts
+++ src/hooks/useRAG.ts
@@ -1,53 +1,57 @@
 import createDebug from 'debug';
-import { useAxios } from '@vueuse/integrations/useAxios';
+import { useWebSockets } from '@/hooks/useWebSockets';
+import { generateUUID } from '@/utils/helpers';
 
 const debug = createDebug('@ccu:hooks:useRAG');
 
 interface RAGEntry {
-  actor: 'user' | 'assistant';
+  messageId: string;
+  actor: 'user' | 'aarongpt';
   content: string;
 }
 
 export const useRAG = () => {
   const history = ref<RAGEntry[]>([]);
+  const latestMessage = computed(() => history.value.at(-1));
+  const latestMessageId = computed(() => latestMessage.value?.messageId);
 
-  const ragState = useAxios<RAGQuery>(
-    '/rag_basic_faq',
-    {
-      method: 'POST',
-      baseURL: import.meta.env.VITE_APP_API_BASE_URL,
-    },
-    {
-      immediate: false,
-      resetOnExecute: true,
+  const socket = useWebSockets<{ answer: string; message_id: string }>(
+    '/ws/rag',
+    'rag',
+    (data) => {
+      debug('Received data from websocket %o', data);
+      const { answer, message_id } = data;
+      if (latestMessageId.value === message_id) {
+        debug('appending latest message chunk: %s (%s)', message_id, answer);
+        const newHistory = history.value.slice(0, -1);
+        const newLatest = latestMessage.value!;
+        newLatest.content += answer;
+        history.value = [...newHistory, newLatest];
+      } else {
+        debug('pushing new message chunk: %s (%s)', message_id, answer);
+        history.value.push({
+          messageId: message_id,
+          actor: 'aarongpt',
+          content: answer,
+        });
+      }
     },
   );
 
   const submitQuestion = (value: string) => {
     if (!value) return;
     history.value.push({
+      messageId: generateUUID(),
       actor: 'user',
       content: value,
     });
-    return ragState.execute(undefined, {
-      data: {
-        question: value,
-      },
+    socket.send({
+      question: value,
     });
   };
-
-  whenever(ragState.data, (newValue) => {
-    if (!newValue) return;
-    debug('Pushing new value to history %o', newValue);
-    history.value.push({
-      actor: 'assistant',
-      content: newValue.answer,
-    });
-  });
 
   return {
     submitQuestion,
     history: readonly(history),
-    loading: ragState.isLoading,
   };
 };
