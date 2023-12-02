c6c328797615f8e08f110815ea8ee1ff730eb6d3
Braden Mars <bradenmars@bradenmars.me>
2023-12-02 04:41:40 -0600

    feat(pages/admin/RAG): render responses as markdown, minor layout improvements
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- src/pages/admin/AdminRAG.vue
+++ src/pages/admin/AdminRAG.vue
@@ -2,25 +2,35 @@
 import { ref } from 'vue';
 import { useRAG } from '@/hooks';
 import BaseInput from '@/components/BaseInput.vue';
+import MarkdownRenderer from '@/components/MarkdownRender.vue';
 
 const question = ref<string>('');
-const { history, submitQuestion, loading } = useRAG();
+const { history, submitQuestion } = useRAG();
 </script>
 
 <template>
-  <div class="grid h-full">
-    <div>
+  <div class="rag grid h-full">
+    <div class="">
       <template v-for="h in history" :key="`${h.actor}:${h.content}`">
         <BaseText variant="h3">{{ h.actor.toUpperCase() }}:</BaseText>
-        <BaseText> {{ h.content }} </BaseText>
+        <MarkdownRenderer :source="h.content" />
+        <br />
       </template>
-      <Spinner v-if="loading" />
     </div>
-    <BaseInput
-      v-model="question"
-      placeholder="Ask a question"
-      class="w-full"
-      @keyup.enter="() => submitQuestion(question)"
-    />
+    <div class="flex items-end">
+      <BaseInput
+        v-model="question"
+        placeholder="Ask a question"
+        class="w-full"
+        @keyup.enter="() => submitQuestion(question)"
+      />
+    </div>
   </div>
 </template>
+
+<style scoped lang="postcss">
+.rag {
+  grid-template-columns: 1fr;
+  grid-template-rows: 1fr 0.1fr;
+}
+</style>
