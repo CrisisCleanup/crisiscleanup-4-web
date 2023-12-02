c363f282ca60c2d018a5e4cee6f524a12d3a3782
Braden Mars <bradenmars@bradenmars.me>
2023-12-02 05:08:50 -0600

    test(hooks/useRAG): update unit tests for useRAG
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- test/unit/hooks/useRAG.test.ts
+++ test/unit/hooks/useRAG.test.ts
@@ -1,41 +1,95 @@
-import { beforeEach, describe, expect, it, vi } from 'vitest';
+import { describe, it, expect, vi, beforeEach, afterEach } from 'vitest';
+import { reactive, nextTick } from 'vue';
 import { useRAG } from '@/hooks/useRAG';
+import { generateUUID } from '@/utils/helpers';
 
-vi.mock('@vueuse/integrations/useAxios', () => {
-  return {
-    useAxios: vi.fn(() => ({
-      data: ref(null),
-      isLoading: ref(false),
-      execute: vi.fn(),
-    })),
-  };
-});
+// Mock dependencies
+vi.mock('@/utils/helpers', () => ({
+  generateUUID: vi.fn().mockReturnValue('mocked-uuid'),
+}));
+
+class MockWebSocket {
+  listeners = {};
+
+  constructor() {
+    this.listeners = {
+      message: [],
+      send: [],
+      close: [],
+    };
+  }
+
+  addEventListener(event, callback) {
+    if (!this.listeners[event]) {
+      this.listeners[event] = [];
+    }
+    this.listeners[event].push(callback);
+  }
+
+  simulateMessage(data) {
+    if (this.listeners['message']) {
+      for (const callback of this.listeners['message']) {
+        callback({ data: JSON.stringify(data) });
+      }
+    }
+  }
+
+  send = vi.fn((data) => {
+    if (this.listeners['send']) {
+      for (const callback of this.listeners['send']) {
+        callback(data);
+      }
+    }
+  });
+
+  close() {
+    if (this.listeners['close']) {
+      for (const callback of this.listeners['close']) callback();
+    }
+  }
+}
+
+const mockWebSocket = new MockWebSocket();
+vi.mock('@/hooks/useWebSockets', () => ({
+  useWebSockets: vi.fn().mockImplementation(() => ({
+    socket: mockWebSocket,
+    send: mockWebSocket.send,
+  })),
+}));
 
 describe('useRAG', () => {
+  let history: ReturnType<typeof useRAG>['history'];
+  let submitQuestion: ReturnType<typeof useRAG>['submitQuestion'];
+
   beforeEach(() => {
     vi.clearAllMocks();
+    const rag = useRAG();
+    history = rag.history;
+    submitQuestion = rag.submitQuestion;
   });
 
-  it('should initialize with empty history', () => {
-    const { history } = useRAG();
+  it('initializes history as an empty array', () => {
     expect(history.value).toEqual([]);
   });
 
-  it('should add user question to history when submitted', async () => {
-    const { submitQuestion, history } = useRAG();
-    const question = 'What is the capital of France?';
-    await submitQuestion(question);
-    expect(history.value).toEqual([{ actor: 'user', content: question }]);
+  it('adds a new question to history when submitQuestion is called', async () => {
+    submitQuestion('Test question');
+    await nextTick();
+    expect(history.value).toHaveLength(1);
+    expect(history.value[0]).toEqual({
+      messageId: 'mocked-uuid',
+      actor: 'user',
+      content: 'Test question',
+    });
+    expect(mockWebSocket.send).toHaveBeenCalledWith({
+      question: 'Test question',
+    });
   });
 
-  it('should not add empty questions to history', async () => {
-    const { submitQuestion, history } = useRAG();
-    await submitQuestion('');
-    expect(history.value).toEqual([]);
-  });
-
-  it('should expose loading state from useAxios', () => {
-    const { loading } = useRAG();
-    expect(loading.value).toBe(false);
+  it('does not add a question to history when submitQuestion is called with an empty string', async () => {
+    submitQuestion('');
+    await nextTick();
+    expect(history.value).toHaveLength(0);
+    expect(mockWebSocket.send).not.toHaveBeenCalled();
   });
 });
