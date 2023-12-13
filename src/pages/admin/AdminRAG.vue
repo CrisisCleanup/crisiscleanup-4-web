f2f89d22fa56ef3ac8ef97610122ebaca1d48bc9
Braden Mars <bradenmars@bradenmars.me>
2023-12-13 05:09:29 -0600

    feat(pages/admin/rag): implement collections selector, chat overflow fix
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- src/pages/admin/AdminRAG.vue
+++ src/pages/admin/AdminRAG.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
-import { ref } from 'vue';
+import { ref, type Ref } from 'vue';
 import { useRAG, useRAGCollections, useRAGUpload } from '@/hooks';
-import { whenever } from '@vueuse/core';
+import { whenever, useElementSize } from '@vueuse/core';
 import BaseInput from '@/components/BaseInput.vue';
 import MarkdownRenderer from '@/components/MarkdownRender.vue';
 import { getErrorMessage } from '@/utils/errors';
@@ -10,15 +10,29 @@ import TitledCard from '@/components/cards/TitledCard.vue';
 import BaseText from '@/components/BaseText.vue';
 
 const question = ref<string>('');
-const { history, submitQuestion } = useRAG();
 const { collections } = useRAGCollections();
 // todo: collection selection
-const collection = computed(
-  () => collections.value?.find?.((c) => c.name === 'crisiscleanup'),
+const collectionId = ref<string | undefined>();
+watchOnce(collections, () => {
+  if (!collection.value) {
+    collectionId.value = collections.value?.find?.(
+      (c) => c.name === 'crisiscleanup',
+    )?.uuid;
+  }
+});
+
+const { history, submitQuestion } = useRAG(collectionId as Ref<string>);
+
+const collectionOptions = computed(() =>
+  collections.value.map((c) => ({ ...c, label: c.name.toUpperCase() })),
 );
-const collectionId = computed(() => collection.value?.uuid);
+
+const collection = computed(
+  () => collections.value?.find((c) => c.uuid === collectionId.value),
+);
+const hasCollection = computed(() => !!collection.value);
 const collectionDocs = computed(() => collection.value?.files);
-const { uploadFile, uploadedDocuments, isLoading } = useRAGUpload(collectionId);
+const { uploadFile, uploadedDocuments } = useRAGUpload(collectionId);
 
 const uploadsQueue = ref<Blob[]>([]);
 whenever(uploadsQueue, async (newValue) => {
@@ -32,17 +46,38 @@ whenever(uploadsQueue, async (newValue) => {
 
 const uploadFiles = async (fileList: Blob[]) =>
   await Promise.all(fileList.map(uploadFile));
+
+const chatDomRef = ref<HTMLElement>();
+const chatDomSize = useElementSize(chatDomRef);
+const chatDomHeight = ref(0);
+whenever(chatDomHeight, (newValue) => {
+  if (chatDomRef.value) {
+    chatDomRef.value.style.maxHeight = `${newValue}px`;
+  }
+});
+onMounted(() => {
+  chatDomHeight.value = chatDomSize.height.value;
+});
+whenever(chatDomSize.height, (newValue) => {
+  chatDomHeight.value = newValue;
+});
 </script>
 
 <template>
   <div class="rag grid grid-cols-2 gap-2 h-full overflow-x-visible">
     <div class="col-span-2">
-      <BaseText variant="h1">
-        {{ collection?.name?.toUpperCase() }}
-      </BaseText>
+      <BaseSelect
+        v-model="collectionId"
+        :placeholder="$t('Select Collection')"
+        :options="collectionOptions"
+        item-key="uuid"
+        label="label"
+        :loading="!hasCollection"
+        :can-deselect="false"
+      />
     </div>
-    <TitledCard title="Chat">
-      <div class="rag--chat">
+    <TitledCard :title="$t('Chat')">
+      <div ref="chatDomRef" class="rag--chat overflow-y-auto">
         <template v-for="h in history" :key="`${h.actor}:${h.content}`">
           <BaseText variant="h3">{{ h.actor.toUpperCase() }}:</BaseText>
           <MarkdownRenderer class="pl-2 overflow-y-auto" :source="h.content" />
