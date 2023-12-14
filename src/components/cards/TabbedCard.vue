81296fec3b856b726c3623f71cb56f6847eb9cdc
Braden Mars <bradenmars@bradenmars.me>
2023-12-14 03:00:03 -0600

    feat(components/cards/TabbedCard): migrate TabbedCard component
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

---
+++ src/components/cards/TabbedCard.vue
@@ -0,0 +1,88 @@
+<script setup lang="ts">
+import { useTabs, type Tab } from '@/hooks';
+import Card from '@/components/cards/Card.vue';
+
+const props = withDefaults(
+  defineProps<{
+    tabs: Tab[];
+    loading: boolean;
+  }>(),
+  {
+    loading: false,
+  },
+);
+
+const { tabs, loading } = toRefs(props);
+
+const tabContainer = ref<null | HTMLElement>(null);
+const tabSelector = ref<null | HTMLElement>(null);
+const { activeIndex, selectorStyle, setTab, state } = useTabs({
+  tabs: tabs,
+  tabSelector,
+  tabContainer,
+});
+</script>
+
+<template>
+  <Card :loading="loading" :overlay="true">
+    <template #header>
+      <div ref="tabContainer" :class="`left tabs card__tabbar`">
+        <div
+          v-for="(t, idx) in state.tabs"
+          :key="t.key"
+          :class="`card__tab tab-item tab-${idx} ${
+            idx === activeIndex ? 'active' : ''
+          }`"
+          @click="() => setTab(idx)"
+        >
+          <base-text variant="h3" weight="600">
+            {{ $t(t.title) }}
+          </base-text>
+        </div>
+        <div ref="tabSelector" class="card__selector" :style="selectorStyle" />
+      </div>
+    </template>
+    <div
+      v-for="(t, idx) in tabs"
+      v-show="idx === activeIndex"
+      :key="t.key"
+      class="flex flex-grow flex-col"
+    >
+      <slot :name="t.key" />
+    </div>
+  </Card>
+</template>
+
+<style lang="postcss" scoped>
+.card {
+  &__tabbar {
+    display: flex;
+    justify-content: flex-start;
+    position: relative;
+    @apply h-full w-full bg-white;
+  }
+
+  &__selector {
+    height: 4px;
+    @apply bg-primary-light;
+    position: absolute;
+    z-index: 99;
+    bottom: 0;
+    width: 100%;
+    display: inline-block;
+    transition: transform 300ms ease-in-out;
+  }
+
+  &__tab {
+    @apply py-4 px-6 text-crisiscleanup-dark-400;
+    position: relative;
+    cursor: pointer;
+    transition: opacity 300ms ease-in-out;
+    opacity: 0.5;
+    &:hover,
+    &.active {
+      opacity: 1;
+    }
+  }
+}
+</style>
