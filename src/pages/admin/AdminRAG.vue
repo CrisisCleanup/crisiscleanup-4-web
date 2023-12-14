81c2de1158d15033bce03f53e7e2e29520ef3a3d
Braden Mars <bradenmars@bradenmars.me>
2023-12-14 03:21:01 -0600

    feat(pages/admin/rag): add conversations selection, store collection/conversation in localstorage, improvements
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- src/pages/admin/AdminRAG.vue
+++ src/pages/admin/AdminRAG.vue
@@ -1,18 +1,36 @@
 <script lang="ts" setup>
 import { ref, type Ref } from 'vue';
-import { useRAG, useRAGCollections, useRAGUpload } from '@/hooks';
-import { whenever, useElementSize } from '@vueuse/core';
+import type { Tab } from '@/hooks';
+import {
+  useRAG,
+  useRAGCollections,
+  useRAGConversations,
+  useRAGUpload,
+} from '@/hooks';
+import { useElementSize, whenever, useStorage } from '@vueuse/core';
 import BaseInput from '@/components/BaseInput.vue';
 import MarkdownRenderer from '@/components/MarkdownRender.vue';
 import { getErrorMessage } from '@/utils/errors';
 import DragDrop from '@/components/DragDrop.vue';
 import TitledCard from '@/components/cards/TitledCard.vue';
+import TabbedCard from '@/components/cards/TabbedCard.vue';
 import BaseText from '@/components/BaseText.vue';
+import { generateUUID } from '@/utils/helpers';
 
 const question = ref<string>('');
 const { collections } = useRAGCollections();
-// todo: collection selection
-const collectionId = ref<string | undefined>();
+const collectionId = useStorage<string | undefined>(
+  'rag:collectionId',
+  undefined,
+  localStorage,
+  { writeDefaults: false, listenToStorageChanges: false },
+);
+const conversationId = useStorage<string | undefined>(
+  'rag:conversationId',
+  undefined,
+  localStorage,
+  { writeDefaults: false, listenToStorageChanges: false },
+);
 watchOnce(collections, () => {
   if (!collection.value) {
     collectionId.value = collections.value?.find?.(
@@ -21,7 +39,20 @@ watchOnce(collections, () => {
   }
 });
 
-const { history, submitQuestion } = useRAG(collectionId as Ref<string>);
+const setConversation = (newConversationId: string) => {
+  console.log('set convo:', newConversationId, 'from:', conversationId.value);
+  conversationId.value = newConversationId;
+};
+
+const { currentConversationEntries, conversations } = useRAGConversations(
+  collectionId as Ref<string>,
+  conversationId,
+);
+const { history, submitQuestion } = useRAG(
+  collectionId as Ref<string>,
+  conversationId as Ref<string>,
+  currentConversationEntries,
+);
 
 const collectionOptions = computed(() =>
   collections.value.map((c) => ({ ...c, label: c.name.toUpperCase() })),
@@ -31,8 +62,12 @@ const collection = computed(
   () => collections.value?.find((c) => c.uuid === collectionId.value),
 );
 const hasCollection = computed(() => !!collection.value);
-const collectionDocs = computed(() => collection.value?.files);
-const { uploadFile, uploadedDocuments } = useRAGUpload(collectionId);
+const {
+  uploadFile,
+  uploadedDocuments,
+  collectionDocuments,
+  isDocumentsLoading,
+} = useRAGUpload(collectionId);
 
 const uploadsQueue = ref<Blob[]>([]);
 whenever(uploadsQueue, async (newValue) => {
@@ -61,6 +96,8 @@ onMounted(() => {
 whenever(chatDomSize.height, (newValue) => {
   chatDomHeight.value = newValue;
 });
+
+const configTabs: Tab[] = [{ key: 'conversation' }, { key: 'files' }];
 </script>
 
 <template>
@@ -79,8 +116,13 @@ whenever(chatDomSize.height, (newValue) => {
     <TitledCard :title="$t('Chat')">
       <div ref="chatDomRef" class="rag--chat overflow-y-auto">
         <template v-for="h in history" :key="`${h.actor}:${h.content}`">
-          <BaseText variant="h3">{{ h.actor.toUpperCase() }}:</BaseText>
-          <MarkdownRenderer class="pl-2 overflow-y-auto" :source="h.content" />
+          <BaseText variant="h3" class="pl-1"
+            >{{ h.actor.toUpperCase() }}:</BaseText
+          >
+          <MarkdownRenderer
+            class="pl-3 py-1 overflow-y-auto"
+            :source="h.content"
+          />
           <br />
         </template>
       </div>
@@ -96,26 +138,60 @@ whenever(chatDomSize.height, (newValue) => {
     </TitledCard>
 
     <div class="flex flex-col">
-      <TitledCard title="Files">
-        <DragDrop
-          class="border bg-white"
-          :choose-title="$t('dragDrop.choose_files')"
-          :drag-title="$t('fileUpload.select_file_upload')"
-          @files="(files) => (uploadsQueue = files)"
-        >
-          <template v-if="uploadsQueue.length > 0">
-            <spinner />
+      <TabbedCard :tabs="configTabs">
+        <template #conversation>
+          <template
+            v-for="conv in conversations?.conversations"
+            :key="conv.conversationId"
+          >
+            <div
+              class="border-2 border-t-0 py-1 hover:bg-crisiscleanup-light-grey transition-all cursor-pointer ws-nowrap text-ellipsis"
+              :class="{
+                'bg-crisiscleanup-light-smoke shadow-inner':
+                  conv.conversationId === conversationId,
+              }"
+              @click="() => setConversation(conv.conversationId)"
+            >
+              <BaseText
+                variant="h4"
+                :bold="conv.conversationId === conversationId"
+                class="p-2 text-ellipsis ws-nowrap"
+                >{{ conv.title.split('user: ')[1] }}</BaseText
+              >
+            </div>
           </template>
-        </DragDrop>
-        <template v-for="doc in collectionDocs" :key="doc.filename">
-          <BaseText variant="body">{{ doc.filenameOriginal }}</BaseText>
+          <div class="mx-auto py-2 transition-all cursor-pointer">
+            <ccu-icon
+              :alt="$t('~~New Conversation')"
+              type="active"
+              with-text
+              size="lg"
+              class="conversation__new"
+              @click="() => setConversation(generateUUID())"
+            />
+          </div>
         </template>
-        <template v-for="doc in uploadedDocuments">
-          <BaseText v-for="docId in doc.documentIds" :key="docId">{{
-            docId
-          }}</BaseText>
+        <template #files>
+          <DragDrop
+            class="border bg-white"
+            :choose-title="$t('dragDrop.choose_files')"
+            :drag-title="$t('fileUpload.select_file_upload')"
+            @files="(files) => (uploadsQueue = files)"
+          >
+            <template v-if="isDocumentsLoading || uploadsQueue.length > 0">
+              <spinner />
+            </template>
+          </DragDrop>
+          <template v-for="doc in collectionDocuments" :key="doc.filename">
+            <BaseText variant="body">{{ doc.filenameOriginal }}</BaseText>
+          </template>
+          <template v-for="doc in uploadedDocuments">
+            <BaseText v-for="docId in doc.documentIds" :key="docId">{{
+              docId
+            }}</BaseText>
+          </template>
         </template>
-      </TitledCard>
+      </TabbedCard>
     </div>
   </div>
 </template>
@@ -135,8 +211,12 @@ whenever(chatDomSize.height, (newValue) => {
 }
 
 :deep(.card .body--inner) {
-  @apply inline-grid grid-rows-2 w-full p-3;
+  @apply inline-grid grid-rows-2 w-full;
   grid-template-rows: 1fr auto;
   grid-template-columns: 1fr;
+}
+.conversation__new:hover {
+  transition: filter 150ms ease-in-out;
+  filter: drop-shadow(2px 4px 6px rgba(0, 0, 0, 0.2));
 }
 </style>
