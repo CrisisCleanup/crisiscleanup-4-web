6a70ebc792c52d073c8db841e853a32079c3c996
Deep Panchal <deep302001@gmail.com>
2023-12-26 12:32:23 -0600

    test(unit): add test notes for BaseSelect, add example on new test case with mousedown event

--- test/unit/components/BaseSelect.test.ts
+++ test/unit/components/BaseSelect.test.ts
@@ -62,6 +62,74 @@ describe('BaseSelect', () => {
     });
     expect(spy).not.toHaveBeenCalled();
   });
+
+  /**
+   * Note: The tests below were broken as we upgraded @vueform/multiselect from v2.6.2 to v2.6.6
+   * v2.6.2 -> v.2.6.3 had major changes which replaced click events with mousedown events.
+   * The skipped tests below are testing internal implementation of the library anyways
+   * so it doesn't make sense to test them.
+   *
+   * @see
+   * https://github.com/vueform/multiselect/compare/2.6.2...2.6.3
+   * https://github.com/vueform/multiselect/issues/387
+   */
+
+  //////////////////////////////////////////////////////////////////////////
+  // Test implementation below which doesn't work with mousedown event
+  //////////////////////////////////////////////////////////////////////////
+  // it.only('should be able to select and clear item', async () => {
+  //   // if (typeof document.activeElement.isEqualNode !== 'function') {
+  //   //   Element.prototype.isEqualNode = function (element) {
+  //   //     // Basic implementation or return true to bypass the check
+  //   //     return this === element;
+  //   //   };
+  //   // }
+  //
+  //   const options = ['tobi', 'aaron', 'deep'];
+  //   const wrapper = mount(BaseSelect, {
+  //     props: {
+  //       options,
+  //     },
+  //   });
+  //   await flushPromises();
+  //
+  //   console.log('Before click');
+  //   console.log(wrapper.html());
+  //
+  //   // Trigger click on the options dropdown
+  //   await wrapper.find('.multiselect-options').trigger('mouseenter');
+  //   await wrapper.find('.multiselect-options').trigger('mousedown');
+  //   // await wrapper.find('.multiselect-caret').trigger('click');
+  //   // await wrapper.find('.multiselect-options').trigger('click');
+  //
+  //   console.log('Before select');
+  //   console.log(wrapper.html());
+  //
+  //   // Trigger click on the first option
+  //   await wrapper.find('.multiselect-option').trigger('click');
+  //
+  //   // Wait for the DOM to update
+  //   await nextTick();
+  //
+  //   console.log('After select');
+  //   console.log(wrapper.html());
+  //
+  //   const selectedElement = wrapper.find('.multiselect-single-label-text');
+  //   expect(selectedElement.exists()).toBeTruthy();
+  //   const selected = selectedElement.text();
+  //   expect(selected).toEqual(options[0]);
+  //
+  //   // Trigger click on the clear button
+  //   await wrapper.find('.multiselect-clear').trigger('click');
+  //
+  //   // Wait for the DOM to update
+  //   await nextTick();
+  //
+  //   const selectedAfterClear = wrapper.find('.multiselect-single-label-text');
+  //   expect(selectedAfterClear.exists()).toBeFalsy();
+  //   expect(wrapper.vm.isInvalid).toEqual(false);
+  // });
+
   it.skip('should be able to select and clear item', async () => {
     const options = ['tobi', 'aaron', 'deep'];
     const wrapper = mount(BaseSelect, {
