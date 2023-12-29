874712249d8013adc7a120e9d7479fdfecaa9f40
Deep Panchal <deep302001@gmail.com>
2023-12-29 15:36:08 -0600

    Revert "ci(e2e): use default slack reporter in playwright config to send messages"
    This reverts commit 89d159c82a50a85d3390db3f52589fe2cf2b8c2e.

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,20 +24,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: process.env.CI
-    ? [
-        ['blob', { outputDir: 'blob-report' }],
-        ['github'],
-        [
-          './node_modules/playwright-slack-report/dist/src/SlackReporter.js',
-          {
-            channels: ['qa'],
-            sendResults: 'always',
-            slackLogLevel: 'debug',
-            disableUnfurl: true,
-            showInThread: true,
-          },
-        ],
-      ]
+    ? [['blob', { outputDir: 'blob-report' }], ['github']]
     : [['html', { open: 'never' }]],
   timeout: 1 * 60 * 1000,
   reportSlowTests: {
