b4c4f1475837e884ca98f5b714146413bd24ead8
Braden Mars <bradenmars@bradenmars.me>
2023-12-30 01:11:56 -0600

    feat(postcss): add `postcssPresetEnv` and `postcssPxToRem` with configs
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- postcss.config.ts
+++ postcss.config.ts
@@ -2,7 +2,28 @@ import autoprefixer from 'autoprefixer';
 import postcssImport from 'postcss-import';
 import tailwindcss from 'tailwindcss';
 import tailwindNesting from 'tailwindcss/nesting';
+import postcssPresetEnv from 'postcss-preset-env';
+import postcssPxToRem from 'postcss-pxtorem';
 
 export default {
-  plugins: [postcssImport, tailwindNesting, tailwindcss, autoprefixer],
+  plugins: [
+    postcssImport,
+    tailwindNesting,
+    tailwindcss,
+    /**
+     * postcss-preset-env
+     * @see http://preset-env.cssdb.org/features
+     */
+    postcssPresetEnv({
+      stage: 1,
+      features: {
+        'nesting-rules': true,
+      },
+    }),
+    autoprefixer,
+    // needs to be post auto-prefixer
+    postcssPxToRem({
+      propList: ['font', 'font-size', 'line-height', 'letter-spacing'],
+    }),
+  ],
 };
