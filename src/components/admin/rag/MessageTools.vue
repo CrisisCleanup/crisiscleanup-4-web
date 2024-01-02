f33453e6df8a939d2cbce5e5a0722fdbcc03c50e
Braden Mars <bradenmars@bradenmars.me>
2024-01-02 04:30:46 -0600

    feat(components/admin/rag/MessageTools): implement overlay for listing all documents utilized in ai response by file
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

---
+++ src/components/admin/rag/MessageTools.vue
@@ -0,0 +1,74 @@
+<script setup lang="ts">
+import { defineProps, ref, computed } from 'vue';
+import { type RAGDocument, type RAGEntry, type RAGToolMessage } from '@/hooks';
+import BaseText from '@/components/BaseText.vue';
+const props = defineProps<{
+  entry: RAGEntry;
+}>();
+
+const messageTools = computed<Record<string, RAGToolMessage[]>>(
+  () => props.entry.tools ?? {},
+);
+const messageDocuments = computed<RAGDocument[]>(() =>
+  Object.values(messageTools.value).flatMap((toolMessage) => {
+    return (toolMessage as unknown as RAGToolMessage).documents;
+  }),
+);
+const documentNames = computed(() => [
+  ...new Set(messageDocuments.value.map((doc) => doc.metadata.filename)),
+]);
+const currentDocument = ref(documentNames.value[0]);
+const selectedDocuments = computed(() =>
+  messageDocuments.value.filter(
+    (doc) => doc.metadata.filename === currentDocument.value,
+  ),
+);
+</script>
+
+<template>
+  <div class="grid grid-cols-3">
+    <div>
+      <div v-for="(docName, idx) in documentNames" :key="docName">
+        <div
+          class="py-2 border-2 border-t-0 hover:bg-crisiscleanup-light-grey transition-all cursor-pointer"
+          :class="{
+            'border-t-2': idx === 0,
+            'bg-crisiscleanup-light-grey': docName === currentDocument,
+          }"
+          @click="() => (currentDocument = docName)"
+        >
+          <BaseText variant="h4" class="pl-1 font-display">{{
+            docName
+          }}</BaseText>
+        </div>
+      </div>
+    </div>
+    <div class="overflow-y-auto col-span-2">
+      <div
+        v-for="doc in selectedDocuments"
+        :key="doc.metadata.elementId"
+        class="px-1"
+      >
+        <BaseText variant="h3" class="pl-1 font-display">{{
+          doc.metadata.elementId ?? doc.metadata.filename
+        }}</BaseText>
+        <div class="pl-1">
+          <BaseText variant="h4">Content</BaseText>
+          <BaseText variant="bodyxsm" class="pl-3 pt-1">
+            {{ doc.pageContent }}
+          </BaseText>
+          <BaseText variant="h4">Metadata</BaseText>
+          <BaseText
+            v-for="[key, value] in Object.entries(doc.metadata)"
+            :key="doc.metadata.filename + key"
+            variant="bodyxsm"
+            class="pl-3"
+          >
+            {{ key }}: {{ value }}
+          </BaseText>
+        </div>
+        <br />
+      </div>
+    </div>
+  </div>
+</template>
