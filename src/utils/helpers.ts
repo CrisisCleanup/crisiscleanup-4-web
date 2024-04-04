6aec54cede3bc6f700041b42048860f9e2af1bb1
Deep Panchal <deep302001@gmail.com>
2024-04-04 09:38:58 -0500

    feat(utils): add getColorContrast helper util
    Signed-off-by: Deep Panchal <deep302001@gmail.com>

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -147,3 +147,23 @@ export const createAxiosCasingTransform = (
   debug('axios options: %o', axiosOptions);
   return axiosOptions as AxiosRequestConfig;
 };
+
+export function getColorContrast(
+  bgColor: string,
+  darkColor = '#000000',
+  lightColor = '#ffffff',
+) {
+  const color = bgColor.charAt(0) === '#' ? bgColor.slice(1, 7) : bgColor;
+  const r = Number.parseInt(color.slice(0, 2), 16); // hexToR
+  const g = Number.parseInt(color.slice(2, 4), 16); // hexToG
+  const b = Number.parseInt(color.slice(4, 6), 16); // hexToB
+  const uicolors = [r / 255, g / 255, b / 255];
+  const c = uicolors.map((col) => {
+    if (col <= 0.039_28) {
+      return col / 12.92;
+    }
+    return Math.pow((col + 0.055) / 1.055, 2.4);
+  });
+  const L = 0.2126 * c[0] + 0.7152 * c[1] + 0.0722 * c[2];
+  return L > 0.179 ? darkColor : lightColor;
+}
