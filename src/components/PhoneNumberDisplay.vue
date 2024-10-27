f74e8262f01d2449367e4e791b440d3a26abebfe
Deep Panchal <deep302001@gmail.com>
2024-10-27 13:52:49 -0600

    fix(comps): use CopyText in PhoneNumberDisplay component
    Signed-off-by: Deep Panchal <deep302001@gmail.com>

--- src/components/PhoneNumberDisplay.vue
+++ src/components/PhoneNumberDisplay.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
-import { useToast } from 'vue-toastification';
-import { useClipboard } from '@vueuse/core';
+import CopyText from '@/components/CopyText.vue';
 import { formatNationalNumber } from '@/filters';
 
 interface PhoneNumberDisplayProps {
@@ -9,48 +8,26 @@ interface PhoneNumberDisplayProps {
 
 const props = defineProps<PhoneNumberDisplayProps>();
 
-const { t } = useI18n();
-const $toasted = useToast();
-
-const phoneNumber = computed(() => props.phoneNumber);
-const { copy, copied } = useClipboard({
-  legacy: true, // copy with execCommand as fallback
-});
-
-async function copyToClipboard(text: string) {
-  await copy(text);
-  $toasted.success(t('info.copied_to_clipboard'), { timeout: 2000 });
-}
+const formattedPhoneNumber = computed(() =>
+  formatNationalNumber(props.phoneNumber),
+);
 </script>
 
 <template>
-  <div class="flex">
+  <CopyText
+    :text="formattedPhoneNumber"
+    icon-class="p-2 rounded-r-full text-sm bg-primary-light bg-opacity-80"
+  >
     <div
-      class="flex items-center bg-primary-light bg-opacity-30 py-1 px-3 rounded-l-full text-xs md:text-sm"
+      class="py-2 px-3 rounded-l-full text-xs md:text-sm bg-primary-light bg-opacity-30"
     >
-      <a :href="`tel:${phoneNumber}`" @click.stop>
-        {{ formatNationalNumber(String(phoneNumber)) }}
+      <a :href="`tel:${props.phoneNumber}`" @click.stop>
+        <span class="text-black underline">
+          {{ formattedPhoneNumber }}
+        </span>
       </a>
     </div>
-    <div
-      v-tooltip="{
-        content: copied ? t('actions.copied') : t('actions.copy_to_clipboard'),
-        showTriggers: ['hover', 'click'],
-        hideTriggers: ['hover'],
-        popperClass: 'interactive-tooltip',
-      }"
-      class="flex items-center cursor-pointer bg-primary-light text-sm bg-opacity-80 p-2 rounded-r-full"
-      @click.stop="copyToClipboard(phoneNumber)"
-    >
-      <span class="flex gap-1 items-center text-xs">
-        <ccu-icon
-          size="sm"
-          fa
-          :type="copied ? 'fa-solid fa-copy' : 'fa-regular fa-copy'"
-        />
-      </span>
-    </div>
-  </div>
+  </CopyText>
 </template>
 
 <style scoped></style>
