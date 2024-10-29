1a783f885a0f3eecc26d62136dca9895746550b3
Deep Panchal <deep302001@gmail.com>
2024-10-29 17:15:00 -0600

    fix(app): use plain PhoneNumberDisplay on PewPew & IncidentContact
    Signed-off-by: Deep Panchal <deep302001@gmail.com>

--- src/pages/unauthenticated/PewPew.vue
+++ src/pages/unauthenticated/PewPew.vue
@@ -148,6 +148,7 @@
                           incident,
                         )"
                         :key="hotlineNumber"
+                        type="plain"
                         :phone-number="hotlineNumber"
                       />
                     </div>
