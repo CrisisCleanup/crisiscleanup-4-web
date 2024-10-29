4cbcbdca000c1fa017496a1c5a90d89d40a3d8b4
Deep Panchal <deep302001@gmail.com>
2024-10-29 11:37:05 -0600

    fix(comps): add ColoredCircle component
    Signed-off-by: Deep Panchal <deep302001@gmail.com>

---
+++ src/components/ColoredCircle.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import MaterialSymbolsCircle from '~icons/material-symbols/circle';
+export interface ColoredCircleProps {
+  color: string;
+}
+const props = defineProps<ColoredCircleProps>();
+</script>
+
+<template>
+  <MaterialSymbolsCircle :style="{ color: props.color }" />
+</template>
+
+<style lang="postcss" scoped></style>
