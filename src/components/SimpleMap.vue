b9c71e2f39852cf2509b018e47c851e938d1ac15
Deep Panchal <deep302001@gmail.com>
2024-10-29 13:52:09 -0600

    refactor(comps): improve WorksiteLegend layout & its usage in SimpleMap
    Signed-off-by: Deep Panchal <deep302001@gmail.com>

--- src/components/SimpleMap.vue
+++ src/components/SimpleMap.vue
@@ -1,16 +1,64 @@
+<script lang="ts" setup>
+import WorksiteLegend from '@/components/WorksiteLegend.vue';
+import MdiChevronUp from '~icons/mdi/chevron-up';
+import MdiChevronDown from '~icons/mdi/chevron-down';
+import useCurrentUser from '@/hooks/useCurrentUser';
+import { getErrorMessage } from '@/utils/errors';
+
+export interface SimpleMapProps {
+  mapLoading?: boolean;
+  zoomButtonsClass?: string;
+  showZoomButtons?: boolean;
+  showLegend?: boolean;
+  removeLegend?: boolean;
+  availableWorkTypes?: Record<string, unknown>[];
+  showMapLayerToggle?: boolean;
+}
+
+export interface SimpleMapEmits {
+  (event: 'onZoomIn'): void;
+  (event: 'onZoomIn'): void;
+  (event: 'onZoomOut'): void;
+  (event: 'onZoomInteractive'): void;
+  (event: 'onZoomIncidentCenter'): void;
+  (event: 'onToggleMapType'): void;
+}
+
+const props = withDefaults(defineProps<SimpleMapProps>(), {
+  zoomButtonsClass: '',
+  availableWorkTypes: () => [],
+});
+
+const emit = defineEmits<SimpleMapEmits>();
+
+const { t } = useI18n();
+const { currentUser, userStates, updateUserStates } = useCurrentUser();
+const showingLegend = ref(userStates.value?.showingLegend ?? true);
+const legendRef = ref<HTMLElement | null>(null);
+
+function toggleLegend(status) {
+  showingLegend.value = status;
+  updateUserStates({ showingLegend: status }, {}).catch(getErrorMessage);
+}
+
+watch(showingLegend, async (newValue) => {
+  await updateUserStates({ showingLegend: newValue }, {});
+});
+</script>
+
 <template>
   <div id="map" ref="map" class="absolute top-0 left-0 right-0 bottom-0 z-50">
     <div
-      v-if="mapLoading"
+      v-if="props.mapLoading"
       class="absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center z-map-controls"
     >
       <spinner show-quote />
     </div>
     <div
-      v-if="showZoomButtons"
+      v-if="props.showZoomButtons"
       class="flex flex-col absolute zoom-control-container z-map-controls"
       style="top: 10px; left: 10px"
-      :class="zoomButtonsClass"
+      :class="props.zoomButtonsClass"
     >
       <div class="zoom-control flex flex-col mb-5">
         <base-button
@@ -23,7 +71,7 @@
           :alt="$t('worksiteMap.zoom_in')"
           :action="
             () => {
-              $emit('onZoomIn');
+              emit('onZoomIn');
             }
           "
           class="w-8 h-8 border-crisiscleanup-dark-100 border-t border-l border-r bg-white shadow-xl text-xl text-crisiscleanup-dark-400"
@@ -38,7 +86,7 @@
           :alt="$t('worksiteMap.zoom_out')"
           :action="
             () => {
-              $emit('onZoomOut');
+              emit('onZoomOut');
             }
           "
           class="w-8 h-8 border border-crisiscleanup-dark-100 bg-white shadow-xl text-xl text-crisiscleanup-dark-400"
@@ -60,7 +108,7 @@
         ccu-event="user_ui-zoom-details"
         :action="
           () => {
-            $emit('onZoomInteractive');
+            emit('onZoomInteractive');
           }
         "
         icon="tree"
@@ -85,7 +133,7 @@
         icon-size="lg"
         :action="
           () => {
-            $emit('onZoomIncidentCenter');
+            emit('onZoomIncidentCenter');
           }
         "
         class="w-8 h-8 border border-crisiscleanup-dark-100 my-1 bg-white shadow-xl text-crisiscleanup-dark-400"
@@ -100,52 +148,66 @@
         :alt="$t('worksiteMap.toggle_map_type')"
         :action="
           () => {
-            $emit('onToggleMapType');
+            emit('onToggleMapType');
           }
         "
         class="w-8 h-8 border-crisiscleanup-dark-100 border-t border-l border-r border-b bg-white shadow-xl text-xl text-crisiscleanup-dark-400 mt-4"
       />
     </div>
-    <WorksiteLegend
+
+    <!-- TODO: Extract to Expandable component -->
+    <div
       v-if="(showZoomButtons && !removeLegend) || showLegend"
-      :key="availableWorkTypes"
-      class="hidden md:block rounded-t-lg absolute bottom-0 left-0 w-72 z-map-controls"
-      data-testid="testShowLegendDiv"
-      :available-work-types="availableWorkTypes"
-    />
+      ref="legendRef"
+      class="worksite-legend pb-6 bg-white border-2 border-crisiscleanup-dark-red"
+    >
+      <div
+        v-if="showingLegend"
+        data-testid="testShowingLegendDiv"
+        class="flex flex-col"
+        @click="toggleLegend(false)"
+      >
+        <div class="expandable-header">
+          <div class="text-base font-bold">
+            {{ $t('worksiteMap.legend') }}
+          </div>
+          <MdiChevronDown
+            class="text-lg"
+            data-testid="testHideLegendIcon"
+            :title="$t('worksiteMap.hide_legend')"
+          />
+        </div>
+        <WorksiteLegend
+          :key="availableWorkTypes"
+          class="px-2"
+          :available-work-types="availableWorkTypes"
+        />
+      </div>
+      <div
+        v-else
+        data-testid="testHiddenLegendDiv"
+        class="expandable-header"
+        @click="toggleLegend(true)"
+      >
+        <div class="text-base font-bold">
+          {{ $t('worksiteMap.legend') }}
+        </div>
+        <MdiChevronUp
+          class="text-lg"
+          data-testid="testShowLegendIcon"
+          :title="$t('worksiteMap.show_legend')"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
-<script lang="ts">
-import WorksiteLegend from './WorksiteLegend.vue';
+<style lang="postcss" scoped>
+.worksite-legend {
+  @apply hidden md:block rounded-t-lg absolute bottom-0 left-0 w-72 z-map-controls;
+}
 
-export default defineComponent({
-  name: 'SimpleMap',
-  components: { WorksiteLegend },
-  props: {
-    mapLoading: {
-      type: Boolean,
-    },
-    zoomButtonsClass: {
-      type: String,
-      default: '',
-    },
-    showZoomButtons: {
-      type: Boolean,
-    },
-    showLegend: {
-      type: Boolean,
-    },
-    removeLegend: {
-      type: Boolean,
-    },
-    availableWorkTypes: {
-      type: Object,
-      default: () => ({}),
-    },
-    showMapLayerToggle: {
-      type: Boolean,
-    },
-  },
-});
-</script>
+.expandable-header {
+  @apply p-2 cursor-pointer flex items-center justify-between;
+}
+</style>
