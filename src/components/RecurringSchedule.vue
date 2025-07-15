7c3a914f6baaea19a9c13da120aa5f7f8971a3b1
pablo014 <angelopablo014@gmail.com>
2025-07-15 16:57:02 -0700

    feat(otherOrg): add popover to get rid of overlap on table

--- src/components/RecurringSchedule.vue
+++ src/components/RecurringSchedule.vue
@@ -122,7 +122,7 @@ import { isEqual } from 'lodash';
 import { useI18n } from 'vue-i18n';
 import { ref, defineComponent, onMounted } from 'vue';
 import moment from 'moment';
-// eslint-disable-next-line import/default
+
 import LitepieDatepicker from 'litepie-datepicker';
 
 export default defineComponent({
