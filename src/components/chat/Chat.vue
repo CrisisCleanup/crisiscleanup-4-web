3a859abc2be1394d5f9b53e39dcb20c22742e02d
Tobi Abiodun <abiodun.tobi@gmail.com>
2025-07-31 14:43:24 -0400

    Tobi develop (#1367)
    * Issue: Add organization logo and link to https://www.crisiscleanup.org/organization/profile on the dashboard so that volunteers can correct information or the logo.
    
    * Fix issues with image
    
    * Enhance Tailwind configuration and update Leaderboard component layout
    
    - Added new height values (192 to 264) to Tailwind configuration for improved responsiveness.
    - Refactored Leaderboard component to replace TitledCard with a custom layout, enhancing styling and structure.
    - Standardized height classes in PhoneOverlay component for consistent UI across views.
    
    * Refactor Chat component layout
    
    - Removed unnecessary height constraint from the Chat component's main div, improving layout flexibility.

--- src/components/chat/Chat.vue
+++ src/components/chat/Chat.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex-1 p-2 sm:p-3 flex flex-col w-full h-full">
+  <div class="flex-1 p-2 sm:p-3 flex flex-col w-full">
     <div class="flex sm:items-center py-1 border-b border-gray-200">
       <div class="text-lg">{{ chat.name }}</div>
     </div>
