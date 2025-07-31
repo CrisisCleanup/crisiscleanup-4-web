871d7a32304ca324f8a6e64547f3796158298e47
Tobi Abiodun <abiodun.tobi@gmail.com>
2025-07-31 14:27:30 -0400

    Tobi develop (#1366)
    * Issue: Add organization logo and link to https://www.crisiscleanup.org/organization/profile on the dashboard so that volunteers can correct information or the logo.
    
    * Fix issues with image
    
    * Enhance Tailwind configuration and update Leaderboard component layout
    
    - Added new height values (192 to 264) to Tailwind configuration for improved responsiveness.
    - Refactored Leaderboard component to replace TitledCard with a custom layout, enhancing styling and structure.
    - Standardized height classes in PhoneOverlay component for consistent UI across views.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -176,6 +176,13 @@ module.exports = {
         156: '39rem',
         168: '42rem',
         180: '45rem',
+        192: '48rem',
+        204: '51rem',
+        216: '54rem',
+        228: '57rem',
+        240: '60rem',
+        252: '63rem',
+        264: '66rem',
       },
       boxShadow: {
         'crisiscleanup-card': '0 4px 8px 0px rgba(164, 177, 184, 0.6)',
